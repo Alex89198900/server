b225cc1de49306c794dbc38cb782a687a7fc87da
Alex89198900 <alexchorn1989@ukr.net>
2023-07-08 16:59:10 +0300

    hfhffh

--- db.ts
+++ db.ts
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 import path from "path";
-import dotenv from "dotenv";
+//import dotenv from "dotenv";
 
-dotenv.config({ path: path.resolve(__dirname, "./env") });
+//dotenv.config({ path: path.resolve(__dirname, "./env") });
 // console.log(process.env.DB_NAME);
 // module.exports = new Sequelize(
 //   process.env.DB_NAME ? process.env.DB_NAME : "online_store", // Название БД
@@ -17,13 +17,6 @@ dotenv.config({ path: path.resolve(__dirname, "./env") });
 // );
 // //require("dotenv").config();
 module.exports = new Sequelize(
-  "online_store", // Название БД
-  "postgres", // Пользователь
-  "gagagigu89", // ПАРОЛЬ
-  {
-    dialect: "postgres",
-    host: "localhost",
-    port: 5432,
-  }
+  "postgres://postgres1:X164gEYWB92WUFr5CPirMEADJIpizss1@dpg-cik39fmnqql0l1vfhppg-a.oregon-postgres.render.com/online_store_daf4?ssl=true"
 );
 
